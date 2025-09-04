f9cd9110e1259c32061d5c1f33549609290869bf
sang.nguyen <sang.nguyen@gmail.com>
2025-09-04 21:57:05 +0700

    Fix save file success

--- run.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("🚀 Đăng Ký Tài Khoản Rakuten")
-    print("=" * 40)
-    # Chạy automation
-    from rakuten_auto_reg import main as run_automation
-    run_automation()
-
-if __name__ == "__main__":
-    main()
